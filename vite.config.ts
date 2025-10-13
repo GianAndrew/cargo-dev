26c83c11801946cea2ae46983ee9cf768d7e2875
Gian Andrew Del Rosario <gian@Gians-MacBook-Air.local>
2025-10-13 20:33:29 +0800

    some changes on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		allowedHosts: ['cargo-ix88r.ondigitalocean.app', 'cargorental.me'],
+	},
 	plugins: [
 		react(),
 		tailwindcss(),
