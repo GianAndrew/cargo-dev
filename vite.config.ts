7781daaa3635faceca1f02b22c5743b3e246890c
Gian Andrew Del Rosario <gian@Gians-MacBook-Air.local>
2025-10-13 20:48:05 +0800

    some changes on building dist

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,21 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+	build: {
+		outDir: 'dist',
+		sourcemap: true,
+		chunkSizeWarningLimit: 1600,
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].[hash].js',
+				chunkFileNames: 'assets/[name].[hash].js',
+				assetFileNames: 'assets/[name].[hash].[ext]',
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				},
+			},
+		},
+	},
 });
