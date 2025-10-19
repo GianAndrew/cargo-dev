a4d989b77473e46102bbff841d9e7e82559a8dd6
Gian Andrew Del Rosario <gian@Gians-MacBook-Air.local>
2025-10-19 21:47:03 +0800

    changed api url

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
-// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://api.cargorental.me';
 
 const useAxios = () => {
 	const token_session = sessionStorage.getItem('token');
